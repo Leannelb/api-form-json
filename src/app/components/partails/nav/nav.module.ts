a9b974382ffccd9159173c60ec2b7292db831696
Leannelb <leannelaceybyrne@outlook.com>
2019-08-06 16:54:24 +0200

    navigation component added, lazy loaded, imported & exported & imported again into home.module

---
+++ src/app/components/partails/nav/nav.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav.component';
+
+@NgModule({
+  declarations: [
+    NavComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavComponent
+  ]
+})
+export class NavModule { }
--- src/app/components/screens/home/home.module.ts
+++ src/app/components/screens/home/home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormModule } from '../../partails/form/form.module';
+import { NavModule } from '../../partails/nav/nav.module';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
@@ -15,7 +16,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    FormModule
+    FormModule,
+    NavModule
   ]
 })
 export class HomeModule { }
