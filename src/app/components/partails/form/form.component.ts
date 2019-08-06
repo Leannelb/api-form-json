490a2779504d8d50ba8fb908c8cfaa5f528033a9
Leannelb <leannelaceybyrne@outlook.com>
2019-08-06 16:49:59 +0200

    partials used across components via import & export in mutual module

---
+++ src/app/components/partails/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'form-partial',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/screens/home/home.module.ts
+++ src/app/components/screens/home/home.module.ts
@@ -2,6 +2,7 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormModule } from '../../partails/form/form.module';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
@@ -13,7 +14,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormModule
   ]
 })
 export class HomeModule { }
